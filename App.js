073e70db3a7a382b8253f32a935a1370075d806a
Adam Albrecht <adam.albrecht@gmail.com>
2017-04-15 00:59:28 -0400

    Commented out main parts of code so they can be added back in

--- App.js
+++ App.js
@@ -1,29 +1,21 @@
+// ======= TODO =============
+/*
+ * 1. Load current location using GPS
+ * 2. Display map coordinates
+ * 3. Map of current location
+ * 4. Add a button to search for Pizza restaurants on Google
+ * 5. Show list of results
+ * 6. Show results on the map
+ */
+
+
 import React, { Component } from 'react';
 import { Text, View, StyleSheet, Button, ActivityIndicator } from 'react-native';
 import { Constants } from 'expo';
 import Colors from './Colors';
-import LoadingScreen from './LoadingScreen';
-import RestaurantMap from './RestaurantMap';
-import RestaurantList from './RestaurantList';
-
-const GOOGLE_PLACES_API_KEY = 'AIzaSyCzqO7J9gx9dXiFEj0FgsJ6f2FoRBC4YXY';
-
-const DEFAULT_MAP_REGION_DELTA = 0.025;
-
-// Lat: 39.997308
-// Lng: -83.036152
-
-function placeSearchUrl(latitude, longitude, keyword = '', radius = 2000) {
-  return `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius}&keyword=${keyword}&key=${GOOGLE_PLACES_API_KEY}`;
-}
-
-/*
- * 1. Show Loading animation until we have current location
- * 2. Once we have the current location (lat/lng), show on Map
- * 3. Search for pizza restaurants nearby
- * 4. Show list of restuarants underneath map
- * 5. Add pins to map
- */
+// import LoadingScreen from './LoadingScreen';
+// import RestaurantMap from './RestaurantMap';
+// import RestaurantList from './RestaurantList';
 
 export default class App extends Component {
   state = {
@@ -34,21 +26,7 @@ export default class App extends Component {
   };
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        this.setState({
-          mapRegion: {
-            latitude: pos.coords.latitude,
-            longitude: pos.coords.longitude,
-            latitudeDelta: DEFAULT_MAP_REGION_DELTA,
-            longitudeDelta: DEFAULT_MAP_REGION_DELTA
-          },
-          mapLoading: false
-        });
-      },
-      (error) => alert(JSON.stringify(error)),
-      { enableHighAccuracy: true, timeout: 20000 }
-    );
+
   }
 
   render() {
@@ -57,46 +35,12 @@ export default class App extends Component {
     } else {
       return (
         <View style={styles.container}>
-          <RestaurantMap
-            region={this.state.mapRegion}
-            onRegionChangeComplete={this.handleMapRegionChange}
-            restaurants={this.state.restaurantResults} />
-          <Text>{ this.displayMapCoordinates() }</Text>
-          <Button
-            onPress={this.searchForPizzaRestaurants}
-            disabled={this.state.isSearching}
-            title={this.state.isSearching ? "Searching..." : "Find Me Some Pizza!"}
-            style={styles.buttonStyles}
-            color={Colors.blue} />
-          { this.state.isSearching ? <ActivityIndicator /> : <RestaurantList restaurants={this.state.restaurantResults} /> }
+          <Text style={{fontWeight: 'bold'}}>Blank Application</Text>
         </View>
       );
     }
   }
 
-  handleMapRegionChange = mapRegion => {
-    this.setState({ mapRegion: mapRegion });
-    // this.searchForRestaurants(mapRegion.latitude, mapRegion.longitude);
-  };
-
-  searchForPizzaRestaurants = () => {
-    var url = placeSearchUrl(this.state.mapRegion.latitude, this.state.mapRegion.longitude, 'pizza');
-    this.setState({ isSearching: true }, function() {
-      fetch(url)
-        .then((response) => response.json())
-        .then((responseJson) => {
-          this.setState({ isSearching: false, restaurantResults: responseJson.results });
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    });
-  }
-
-  displayMapCoordinates() {
-    var r = 10000;
-    return `Lat: ${(Math.round(this.state.mapRegion.latitude * r) / r)}, Lng: ${(Math.round(this.state.mapRegion.longitude * r) / r)}`;
-  }
 }
 
 const styles = StyleSheet.create({
@@ -107,3 +51,90 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.bgColor,
   }
 });
+
+// ----- 4C ------
+// const GOOGLE_PLACES_API_KEY = 'AIzaSyCzqO7J9gx9dXiFEj0FgsJ6f2FoRBC4YXY';
+// function placeSearchUrl(latitude, longitude, keyword = '', radius = 2000) {
+//   return `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=${radius}&keyword=${keyword}&key=${GOOGLE_PLACES_API_KEY}`;
+// }
+
+
+// componentDidMount
+
+    // ------- 1 ---------
+    // navigator.geolocation.getCurrentPosition(
+    //   (pos) => {
+    //     this.setState({
+    //       mapRegion: {
+    //         latitude: pos.coords.latitude,
+    //         longitude: pos.coords.longitude,
+    //         latitudeDelta: 0.025,
+    //         longitudeDelta: 0.025
+    //       },
+    //       mapLoading: false
+    //     });
+    //   },
+    //   (error) => alert(JSON.stringify(error)),
+    //   { enableHighAccuracy: true, timeout: 20000 }
+    // );
+
+
+// render
+
+          // ----- 3A ------
+          // <RestaurantMap
+          //   region={this.state.mapRegion}
+          //   onRegionChangeComplete={this.handleMapRegionChange}
+          //   restaurants={this.state.restaurantResults} />
+          // ----- 2A ------
+          // <Text>{ this.displayMapCoordinates() }</Text>
+          // ----- 4A ------
+          // <Button
+          //   onPress={this.searchForPizzaRestaurants}
+          //   disabled={this.state.isSearching}
+          //   title={this.state.isSearching ? "Searching..." : "Find Me Some Pizza!"}
+          //   style={styles.buttonStyles}
+          //   color={Colors.blue} />
+          // ----- 5 ------
+          // { this.state.isSearching ? <ActivityIndicator /> : <RestaurantList restaurants={this.state.restaurantResults} /> }
+
+
+// Helper Methods
+
+  // ------- 3B -------
+  // handleMapRegionChange = mapRegion => {
+  //   this.setState({ mapRegion: mapRegion });
+  // };
+
+  // ------- 4B -------
+  // searchForPizzaRestaurants = () => {
+  //   var url = placeSearchUrl(this.state.mapRegion.latitude, this.state.mapRegion.longitude, 'pizza');
+  //   this.setState({ isSearching: true }, function() {
+  //     fetch(url)
+  //       .then((response) => response.json())
+  //       .then((responseJson) => {
+  //         this.setState({ isSearching: false, restaurantResults: responseJson.results });
+  //       })
+  //       .catch((error) => {
+  //         console.error(error);
+  //       });
+  //   });
+  // }
+
+  // ------ 2B ---------
+  // displayMapCoordinates() {
+  //   var r = 10000;
+  //   return `Lat: ${(Math.round(this.state.mapRegion.latitude * r) / r)}, Lng: ${(Math.round(this.state.mapRegion.longitude * r) / r)}`;
+  // }
+
+
+
+
+
+
+
+
+
+
+// Lat: 39.997308
+// Lng: -83.036152
